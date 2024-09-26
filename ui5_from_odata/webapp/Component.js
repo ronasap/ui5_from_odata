3821e7d9c1b176b09fb85f98b63dc5b43dcb3103
ronasap <rogovnatalia87@gmail.com>
2024-09-26 17:56:27 +0300

    Add files via upload

---
+++ ui5_from_odata/webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ui5_from_odata_srv.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ui5_from_odata_srv.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
